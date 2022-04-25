cdf52b2cce41cda1efa03338fd5eda38ccc3e3e5
Avinash DSP <19211a0417@bvrit.ac.in>
2022-04-25 11:02:34 +0530

    +1 previous commit

---
+++ 5/fir_lpf/fir_lpf.m
@@ -0,0 +1,14 @@
+clc;
+clear all;
+close all;
+n=25;
+wc=0.5;
+
+b=fir1(n,wc);
+[h,w]=freqz(b,1);
+m=20*log(abs(h));
+
+plot(w,m);
+xlabel('Frequency');
+ylabel('Gains in dB');
+title('Magnitude of FIR LPF');
